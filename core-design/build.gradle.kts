2b8be4232ffe5c0a9cf27def3ea876b64f93aaf7
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-07-30 10:51:18 +0330

    Update build.gradle.kts

--- core-design/build.gradle.kts
+++ core-design/build.gradle.kts
@@ -27,6 +27,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.core.ktx)
+
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.graphics)
