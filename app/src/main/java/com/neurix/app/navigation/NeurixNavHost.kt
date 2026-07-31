0a3b8d570ba2e9f85067512061c373c922810c9a
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-07-31 11:50:53 +0330

    Update NeurixNavHost.kt

--- app/src/main/java/com/neurix/app/navigation/NeurixNavHost.kt
+++ app/src/main/java/com/neurix/app/navigation/NeurixNavHost.kt
@@ -1,5 +1,4 @@
-package com.neurix.app.navigation
-
+package com.neurix.navigation
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
