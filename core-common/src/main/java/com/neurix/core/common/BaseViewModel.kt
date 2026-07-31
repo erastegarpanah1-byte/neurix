bf1e66bc1c0866c10e80c1baaaf2751c0d9dc2d5
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-07-31 15:17:22 +0330

    Update BaseViewModel.kt

--- core-common/src/main/java/com/neurix/core/common/BaseViewModel.kt
+++ core-common/src/main/java/com/neurix/core/common/BaseViewModel.kt
@@ -1,24 +1,33 @@
 package com.neurix.core.common
 
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-abstract class BaseViewModel<State : MviState, Intent : MviIntent, Effect : MviEffect>(
+abstract class BaseViewModel<
+        State : MviState,
+        Intent : MviIntent,
+        Effect : MviEffect
+>(
     initialState: State
 ) {
+
     private val _state = MutableStateFlow(initialState)
-    val state: Flow<State> = _state.asStateFlow()
+    val state: StateFlow<State> = _state.asStateFlow()
 
     private val _effect = MutableSharedFlow<Effect>()
-    val effect: Flow<Effect> = _effect.asSharedFlow()
+    val effect: SharedFlow<Effect> = _effect.asSharedFlow()
 
-    val currentState: State get() = _state.value
+    val currentState: State
+        get() = _state.value
 
-    protected fun setState(reduce: State.() -> State) {
-        _state.value = _state.value.reduce()
+    protected fun setState(
+        reducer: State.() -> State
+    ) {
+        _state.value = _state.value.reducer()
     }
 
     protected fun sendEffect(effect: Effect) {
