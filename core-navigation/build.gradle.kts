42ac5dc7852c90d9ca2a959c23d3159f31529f00
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-07-31 11:53:42 +0330

    Update build.gradle.kts

--- core-navigation/build.gradle.kts
+++ core-navigation/build.gradle.kts
@@ -38,8 +38,7 @@ dependencies {
     implementation(libs.compose.ui)
     implementation(libs.compose.material3)
     implementation(libs.compose.animation)
-    implementation("androidx.navigation:navigation-compose:2.8.0")
-    implementation("androidx.compose.animation:animation")
+
 
     // Navigation
     implementation(libs.navigation.compose)
