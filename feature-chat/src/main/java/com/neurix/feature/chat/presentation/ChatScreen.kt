99f09e65c50612bedda62cbcc723b7f02656a188
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-07-31 15:28:46 +0330

    Update ChatScreen.kt

--- feature-chat/src/main/java/com/neurix/feature/chat/presentation/ChatScreen.kt
+++ feature-chat/src/main/java/com/neurix/feature/chat/presentation/ChatScreen.kt
@@ -27,8 +27,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Mic
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.automirrored.filled.Sendimport androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
